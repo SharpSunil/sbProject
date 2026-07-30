c275fee5267a57759d48a81779c92e1881edba5a
sunil shelke <shelkesunil072@gmail.com>
2026-07-30 21:51:50 +0530

    new

--- src/componant/Home_blog/Home_blog.jsx
+++ src/componant/Home_blog/Home_blog.jsx
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, { useRef, useState } from "react";
 import Button from '../button/Buttonmain'
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
 import "./Home_blog.scss"
+import { Link } from "react-router-dom";
+import img1 from "../../assets/1.jpg"
 
 const Home_blog = () => {
   return (
@@ -22,7 +29,162 @@ const Home_blog = () => {
               <Button text="Let's Connect" link="/blog" />
             </div>
             <div className="right-side">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto ipsum, tenetur delectus laudantium nesciunt, magni molestiae ad repellat accusamus dignissimos similique, at perferendis. Quisquam ipsum quae magnam, quia eveniet accusamus.
+              <Swiper className="mySwiper">
+                <SwiperSlide>
+
+                  <div className="card-main">
+                    <div className="card-left">
+                      <Link to="/" className="title">How to create a stunnig</Link>
+                      <div className="card-desc">Explore our latest blog and articles for expert insights on web design,</div>
+                      <div className="details">15 MAY 2026 / BY SUNIL SHELKE</div>
+                    </div>
+                    <div className="card-right">
+                      <img src={img1} />
+                    </div>
+                  </div>
+                </SwiperSlide>
+
+                <SwiperSlide>
+
+                  <div className="card-main">
+                    <div className="card-left">
+                      <Link to="/" className="title">How to create a stunnig</Link>
+                      <div className="card-desc">Explore our latest blog and articles for expert insights on web design,</div>
+                      <div className="details">15 MAY 2026 / BY SUNIL SHELKE</div>
+                    </div>
+                    <div className="card-right">
+                      <img src={img1} />
+                    </div>
+                  </div>
+                </SwiperSlide>
+
+                <SwiperSlide>
+
+                  <div className="card-main">
+                    <div className="card-left">
+                      <Link to="/" className="title">How to create a stunnig</Link>
+                      <div className="card-desc">Explore our latest blog and articles for expert insights on web design,</div>
+                      <div className="details">15 MAY 2026 / BY SUNIL SHELKE</div>
+                    </div>
+                    <div className="card-right">
+                      <img src={img1} />
+                    </div>
+                  </div>
+                </SwiperSlide>
+
+                <SwiperSlide>
+
+                  <div className="card-main">
+                    <div className="card-left">
+                      <Link to="/" className="title">How to create a stunnig</Link>
+                      <div className="card-desc">Explore our latest blog and articles for expert insights on web design,</div>
+                      <div className="details">15 MAY 2026 / BY SUNIL SHELKE</div>
+                    </div>
+                    <div className="card-right">
+                      <img src={img1} />
+                    </div>
+                  </div>
+                </SwiperSlide>
+
+                <SwiperSlide>
+
+                  <div className="card-main">
+                    <div className="card-left">
+                      <Link to="/" className="title">How to create a stunnig</Link>
+                      <div className="card-desc">Explore our latest blog and articles for expert insights on web design,</div>
+                      <div className="details">15 MAY 2026 / BY SUNIL SHELKE</div>
+                    </div>
+                    <div className="card-right">
+                      <img src={img1} />
+                    </div>
+                  </div>
+                </SwiperSlide>
+
+                <SwiperSlide>
+
+                  <div className="card-main">
+                    <div className="card-left">
+                      <Link to="/" className="title">How to create a stunnig</Link>
+                      <div className="card-desc">Explore our latest blog and articles for expert insights on web design,</div>
+                      <div className="details">15 MAY 2026 / BY SUNIL SHELKE</div>
+                    </div>
+                    <div className="card-right">
+                      <img src={img1} />
+                    </div>
+                  </div>
+                </SwiperSlide>
+
+                <SwiperSlide>
+
+                  <div className="card-main">
+                    <div className="card-left">
+                      <Link to="/" className="title">How to create a stunnig</Link>
+                      <div className="card-desc">Explore our latest blog and articles for expert insights on web design,</div>
+                      <div className="details">15 MAY 2026 / BY SUNIL SHELKE</div>
+                    </div>
+                    <div className="card-right">
+                      <img src={img1} />
+                    </div>
+                  </div>
+                </SwiperSlide>
+
+                <SwiperSlide>
+
+                  <div className="card-main">
+                    <div className="card-left">
+                      <Link to="/" className="title">How to create a stunnig</Link>
+                      <div className="card-desc">Explore our latest blog and articles for expert insights on web design,</div>
+                      <div className="details">15 MAY 2026 / BY SUNIL SHELKE</div>
+                    </div>
+                    <div className="card-right">
+                      <img src={img1} />
+                    </div>
+                  </div>
+                </SwiperSlide>
+
+                <SwiperSlide>
+
+                  <div className="card-main">
+                    <div className="card-left">
+                      <Link to="/" className="title">How to create a stunnig</Link>
+                      <div className="card-desc">Explore our latest blog and articles for expert insights on web design,</div>
+                      <div className="details">15 MAY 2026 / BY SUNIL SHELKE</div>
+                    </div>
+                    <div className="card-right">
+                      <img src={img1} />
+                    </div>
+                  </div>
+                </SwiperSlide>
+
+                <SwiperSlide>
+
+                  <div className="card-main">
+                    <div className="card-left">
+                      <Link to="/" className="title">How to create a stunnig</Link>
+                      <div className="card-desc">Explore our latest blog and articles for expert insights on web design,</div>
+                      <div className="details">15 MAY 2026 / BY SUNIL SHELKE</div>
+                    </div>
+                    <div className="card-right">
+                      <img src={img1} />
+                    </div>
+                  </div>
+                </SwiperSlide>
+
+                <SwiperSlide>
+
+                  <div className="card-main">
+                    <div className="card-left">
+                      <Link to="/" className="title">How to create a stunnig</Link>
+                      <div className="card-desc">Explore our latest blog and articles for expert insights on web design,</div>
+                      <div className="details">15 MAY 2026 / BY SUNIL SHELKE</div>
+                    </div>
+                    <div className="card-right">
+                      <img src={img1} />
+                    </div>
+                  </div>
+                </SwiperSlide>
+
+              </Swiper>
             </div>
           </div>
         </div>
