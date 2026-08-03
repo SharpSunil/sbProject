932e38f0abf78d6ae9cb6a2060317203ffee5e7f
Sunil Shelke <shelkesunil072@gmail.com>
2026-08-03 18:33:54 +0530

    new

--- src/componant/Page_compo/About/Fifth_Sec.jsx/Fifth_Sec.jsx
+++ src/componant/Page_compo/About/Fifth_Sec.jsx/Fifth_Sec.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
-
+import Button from '../../../button/Buttonmain'
+import img1 from "../../../../assets/1.jpg"
+import "./Fifth_sec.scss"
 const Fifth_Sec = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <>
+            <div className="fifth-sec-parent parent">
+                <div className="left-sec">
+                    <img src={img1} alt="" />
+                </div>
+                <div className="right-sec">
+                   <div className="content-box-cont cont">
+                     <div className="heading">
+                     The Visionaries Behind the Brands
+                    </div>
+                    <div className="desc">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+                        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+                    </div>
+                    <Button text="Join Our Team" link="/team" />
+                   </div>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default Fifth_Sec
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,6 +4,7 @@ import First_sec from '../componant/Page_compo/About/Timeline/First_Sec/First_se
 import Second_sec from '../componant/Page_compo/About/Timeline/Second_sec/Second_sec'
 import Third_Sec from '../componant/Page_compo/About/Timeline/Third_sec/Third_Sec'
 import Forth_Sec from '../componant/Page_compo/About/Timeline/Forth_sec/Forth_Sec'
+import Fifth_Sec from '../componant/Page_compo/About/Fifth_Sec.jsx/Fifth_Sec'
 // import glow from "../../assets/background_stuck/glow.png";
 
 
@@ -14,6 +15,7 @@ const About = () => {
             <Second_sec />
             <Third_Sec />
             <Forth_Sec />
+            <Fifth_Sec />
 
         </>
     )
